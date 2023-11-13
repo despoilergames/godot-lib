d31bc76c62cd645f7c7fd5e5cf814fbad388ed49
Matt Edmonston <matt.edmonston@gmail.com>
2023-11-13 17:10:00 +0000

    Update chase_camera_2d.gd

--- 2d/chase_camera_2d.gd
+++ 2d/chase_camera_2d.gd
@@ -3,6 +3,13 @@ class_name ChaseCamera2D extends Camera2D
 @export var targets: Array[Node2D]
 @export var weight: float = 0.5
 @export var use_mouse_position: bool = false
+@export var max_distance: float = 0
+
+var _parent: Node2D
+
+func _ready() -> void:
+	_parent = get_parent()
+
 
 func _physics_process(delta: float) -> void:
 	var new_position := Vector2.ZERO
@@ -15,4 +22,9 @@ func _physics_process(delta: float) -> void:
 			continue
 		new_position += to_local(target.global_position)
 	
-	position = new_position * weight
+	new_position *= weight
+	
+	if max_distance and abs(new_position.length()) > max_distance:
+		new_position = (Vector2.RIGHT * max_distance).rotated(new_position.angle())
+	
+	position = new_position
