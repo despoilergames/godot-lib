942cde941f91db423605eb1358f9f91bc47d77f5
Matt Edmonston <matt.edmonston@gmail.com>
2024-02-21 15:39:05 +0000

    Better hutbox 3d timer handling.

--- 3d/hurtbox_3d.gd
+++ 3d/hurtbox_3d.gd
@@ -4,14 +4,16 @@ class_name Hurtbox3D extends Area3D
 @export var interval: float
 @export var damage_on_enter: bool = true
 
-var _timer: Timer = Timer.new()
+var _timer: Timer
 var _areas: Array[Hitbox3D] = []
 
 func _ready() -> void:
-	_timer.wait_time = interval
-	_timer.timeout.connect(_on_timer_timeout)
-	_timer.autostart = false
-	add_child(_timer)
+	if interval:
+		_timer = Timer.new()
+		_timer.wait_time = interval
+		_timer.timeout.connect(_on_timer_timeout)
+		_timer.autostart = false
+		add_child(_timer)
 	
 	area_entered.connect(_on_area_entered)
 	area_exited.connect(_on_area_exited)
@@ -21,7 +23,7 @@ func _on_timer_timeout() -> void:
 	for area in _areas:
 		area.apply_damage(damage)
 	
-	if _areas.is_empty():
+	if _timer and _areas.is_empty():
 		_timer.stop()
 
 
@@ -32,12 +34,12 @@ func _on_area_entered(area: Area3D) -> void:
 		if damage_on_enter:
 			area.apply_damage(damage)
 		
-		if _timer.is_stopped() and interval:
-			_timer.start()
+		if _timer and _timer.is_stopped():
+				_timer.start()
 
 
 func _on_area_exited(area: Area3D) -> void:
 	if area is Hitbox3D:
 		_areas.erase(area)
-		if _areas.is_empty():
+		if _areas.is_empty() and _timer:
 			_timer.stop()
